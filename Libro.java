78fa652714171a86b51987b939c355766c4e5228
Angel Suarez Perez <angelsupe5@gmail.com>
2020-11-06 11:51:41 +0100

    1847.03 Hay un nuevo atributo del numero de paginas

--- Libro.java
+++ Libro.java
@@ -12,22 +12,28 @@ public class Libro {
 
     private String autor;
     private String titulo;
-
+    private int numeroPaginas;
+    
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numPag)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numeroPaginas = numPag;
     }
     
     public String getAutor() {
         return autor;
     }
     
-    public String titulo() {
+    public String getTitulo() {
         return titulo;
+    }
+    
+    public int getNumeroPaginas() {
+        return numeroPaginas; 
     }
     
     public void imprimeAutor() {
